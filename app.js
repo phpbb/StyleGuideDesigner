b8861da9e54f9aa15f359e628eddf7d3f3b64562
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-05-28 03:23:43 +0530

    Basic folder layout & dependencies

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+
+var app = express();

58c860f2d3e6d8b7d67c50b4bf580e84db0e136e
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-05 14:31:38 +0530

    Set up the server & env variables

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var path = require('path');
-var expressValidator = require('express-validator');
+var http = require('http');
 
 var app = express();
+
+//all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});

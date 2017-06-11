ff4be389483e442c52322ccac32820cacfb825cd
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-12 00:58:48 +0530

    fixed build error

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ app.get('/settings', function (req, res) {
 });
 
 app.listen(app.get('port'), function () {
-	console.log('Express server listening on port ' + app.get('port'))
-}
+	console.log('Express server listening on port ' + app.get('port'));
+});

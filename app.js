5117bdc59cd72cc0b060d15aee7b6233e501726a
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-12 00:52:55 +0530

    added a get port functionality to listen

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 
 // All environments
+app.set('port', process.env.PORT || 3000);
 app.set('views', './views');
 app.set('view engine', 'pug');
 
@@ -27,4 +28,6 @@ app.get('/settings', function (req, res) {
 	res.render('settings');
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'))
+}

78a88cbdd1a73edf4a0484b61dba29e1428da531
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-20 16:59:02 +0530

    added vue-resource cdn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
 window.onload = function () {
-	var Vue = require('vue');
-	var Resource = require('vue-resource');
-	Vue.use(Resource);
-
 	var app = new Vue({
 		el: '.app',
 		data: function () {
 			return {
-				message: message
+				message: ""
 			};
 		},
 		created: function () {
+			console.log("hey");
 			this.fetchData()
 		},
 		methods: {
 			fetchData: function () {
-				var apiURL = 'http://localhost:3000/test.json'
+				var apiURL = 'http://localhost:3000/test.json';
+				console.log("hey!");
 				this.$http.get(apiURL, function (data) {
 					console.log('Yo!')
 					this.$set('message', JSON.parse(data))

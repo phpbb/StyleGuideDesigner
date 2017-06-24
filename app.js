01df5c853611194d3a0861355b1c24b579e5b021
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-25 01:54:29 +0530

    different way of reading json file

--- app.js
+++ app.js
@@ -19,11 +19,19 @@ app.set('view engine', 'pug');
 // Set static path
 app.use(express.static(path.join(__dirname, 'assets')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/', home);
 app.use('/demo', demo);
 app.use('/settings', settings);
 app.use('/documentation', documentation);
 
-app.listen(app.get('port'), function () {
-	console.log('Express server listening on port ' + app.get('port'));
+// Express Server Setup
+app.listen(app.get('port'), function (err) {
+	if(err) {
+		console.log('Express Server Error!');
+	} else {
+		console.log('Express server listening on port ' + app.get('port'));
+	}
 });
--- app/controller/settings.js
+++ app/controller/settings.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
+var jsonString = fs.readFileSync('./app/test.json', 'utf8');
+var jsonObj = JSON.parse(jsonString);
+
+/* GET settings page*/
 router.get('/', function (req, res, next) {
-	res.render('settings');
+	res.render('settings', {config: jsonObj.message});
 })
 
 module.exports = router;

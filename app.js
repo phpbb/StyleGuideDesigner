18a28335f156b858253130ba504df9281bf50c77
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-27 01:25:42 +0530

    created CRUD routes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var express = require('express');
-var fs = require('fs');
 var bodyParser = require('body-parser');
 
 var app = express();
--- app/controller/settings.js
+++ app/controller/settings.js
@@ -3,9 +3,34 @@ var router = express.Router();
 
 var jsonObj = require('../test.json');
 
-/* GET settings page*/
+// Route to CREATE a config
+router.post('/configs', function (req, res) {
+	res.json({response: "You sent a post request!"});
+});
+
+// Route to READ all the configs
+router.get('/configs', function (req, res) {
+	res.json({response: "You sent a get request!"});
+});
+
+// Route to READ a single config
+router.get('/configs/:configID', function (req, res) {
+	res.json({response: "You sent a get request with ID - " + req.params.configID});
+});
+
+// Route to UPDATE a single config with the corresponding ID
+router.put('/configs/:configID', function (req, res) {
+	res.json({response: "You sent a update request with ID - " + req.params.configID});
+});
+
+// Route to DELETE a single config with the corresponding ID
+router.delete('/configs/:configID', function (req, res) {
+	res.json({response: "You sent a delete request with ID - " + req.params.configID});
+});
+
+/* Render settings page*/
 router.get('/', function (req, res, next) {
-	res.render('settings', {config: jsonObj.message});
+	res.render('settings');
 })
 
 module.exports = router;

c8d39f245c26c7901b32043b8b6efb4686da8c4c
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-28 20:10:49 +0530

    added ajax get method in vue

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,22 @@ window.onload = function() {
 				{ id: 2, text: 'Extension' }
 				]
 			}
+		},
+		created () {
+			this.loadConfig();
+		},
+		methods: {
+			loadConfig: function() {
+				var xhttp = new XMLHttpRequest();
+				xhttp.onreadystatechange = function() {
+					if (this.readyState == 4 && this.status == 200) {
+						document.getElementById("configDemo").innerHTML = this.responseText;
+					}
+				};
+				xhttp.open("GET", "./test.json", true);
+				xhttp.send();
+			}
+
 		}
 	})
 }

c0ba51f75cde3945dcb8b0cb39125070d4eb8032
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-07-28 01:45:42 +0530

    added temporary sidebar contents

--- app/controller/sidebar.js
+++ app/controller/sidebar.js
@@ -5,18 +5,8 @@ var fs = require('fs');
 
 var sidebar = module.exports = {};
 
-
-// Logic to Read an array files inside a directory
-sidebar.getDir = function () {
-	var readFile = fs.readdir('tests/all/scss/objects', function(err, items) {
-		for (var i=0; i<items.length; i++) {
-			console.log(items[i]);
-		}
-	});
-};
-
-// Logic to read file
-sidebar.getFile = function () {
-	var readFile = fs.readdir('tests/all/scss/objects/_button.scss', 'utf8');
-	return readFile;
+// Logic to read an array files inside a directory
+sidebar.get = function () {
+	var data = fs.readdirSync('tests/all/scss');
+	return data;
 };

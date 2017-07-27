9a6e2d0a9d861266a6099509d404b904b4712346
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-07-28 03:46:41 +0530

    removed build errors-2

--- app/controller/sidebar.js
+++ app/controller/sidebar.js
@@ -10,12 +10,12 @@ sidebar.get = function () {
 	var data1 = fs.readdirSync('tests/all/scss');
 	var data2 = fs.readdirSync('tests/prosilver/scss');
 	var newData = [];
-	for (var i=0; i<data1.length; i++) {
+	for (var i = 0; i < data1.length; i++) {
 		if (data1[i] === 'base' || data1[i] === 'components' || data1[i] === 'objects' || data1[i] === 'settings') {
 			newData.push(data1[i]);
 		}
 	}
-	for (var j=0; j<data2.length; j++) {
+	for (var j = 0; j < data2.length; j++) {
 		if (data2[j] === 'theme' || data2[j] === 'components') {
 			newData.push(data2[j]);
 		}

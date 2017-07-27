a30ae74889079a1f93ce94b1753b26f73d4d14fb
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-07-28 02:17:06 +0530

    added prosilver layers to sidebar

--- app/controller/sidebar.js
+++ app/controller/sidebar.js
@@ -7,11 +7,18 @@ var sidebar = module.exports = {};
 
 // Logic to read an array files inside a directory
 sidebar.get = function () {
-	var data = fs.readdirSync('tests/all/scss');
+	var data1 = fs.readdirSync('tests/all/scss');
+	var data2 = fs.readdirSync('tests/prosilver/scss');
 	var newData = [];
-	for(var i=0; i<data.length; i++) {
-		if(data[i] == 'base' || data[i] == 'components' || data[i] == 'objects' || data[i] == 'settings') {
-			newData.push(data[i]);
+	for(var i=0; i<data1.length; i++) {
+		if(data1[i] == 'base' || data1[i] == 'components' || data1[i] == 'objects' || data1[i] == 'settings') {
+			newData.push(data1[i]);
+		}
+	}
+	console.log(data2);
+	for(var j=0; j<data2.length; j++) {
+		if(data2[j] == 'theme' || data2[j] == 'components') {
+			newData.push(data2[j]);
 		}
 	}
 	return newData;

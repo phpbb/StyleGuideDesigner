a11886a1c9e386c44fbe6a18f04ffc0ef0a1db36
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-07-28 13:52:31 +0530

    refined the controller

--- app/controller/sidebar.js
+++ app/controller/sidebar.js
@@ -1,28 +1,29 @@
 'use strict';
 
-var model = require('../models/model.js');
+var model = require('../models/sidebar.js');
 
 var sidebar = module.exports = {};
 
+sidebar.newData = [];
+
 // Logic to read an array files inside a directory
 sidebar.get = function () {
-	var newData = [];
 	var allData = model.data1;
 	var prosilverData = model.data2;
 	for (var i = 0; i < allData.length; i++) {
 		if (allData[i] === 'base' || allData[i] === 'settings' || allData[i] === 'objects' || allData[i] === 'components') {
-			newData.push(allData[i]);
+			sidebar.newData.push(allData[i]);
 		}
 	}
-	newData.push('--------------');
+	sidebar.newData.push('--------------');
 	for (var j = 0; j < prosilverData.length; j++) {
 		if (prosilverData[j] === 'theme' || prosilverData[j] === 'components' || prosilverData[j] === 'objects') {
-			newData.push(prosilverData[j]);
+			sidebar.newData.push(prosilverData[j]);
 		}
 	}
-	var swap = newData[1];
-	newData[1] = newData[3];
-	newData[3] = swap;
+	var swap = sidebar.newData[1];
+	sidebar.newData[1] = sidebar.newData[3];
+	sidebar.newData[3] = swap;
 
-	return newData;
+	return sidebar.newData;
 };

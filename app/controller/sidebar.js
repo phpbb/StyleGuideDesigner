198001e370203794ea5c5fe86389c1bbc7defc42
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-07-28 12:04:10 +0530

    added objects from prosilver

--- app/controller/sidebar.js
+++ app/controller/sidebar.js
@@ -9,14 +9,13 @@ sidebar.get = function () {
 	var newData = [];
 	var allData = model.data1;
 	var prosilverData = model.data2;
-
 	for (var i = 0; i < allData.length; i++) {
-		if (allData[i] === 'base' || allData[i] === 'components' || allData[i] === 'objects' || allData[i] === 'settings') {
+		if (allData[i] === 'base' || allData[i] === 'settings' || allData[i] === 'objects' || allData[i] === 'components') {
 			newData.push(allData[i]);
 		}
 	}
 	for (var j = 0; j < prosilverData.length; j++) {
-		if (prosilverData[j] === 'theme' || prosilverData[j] === 'components') {
+		if (prosilverData[j] === 'theme' || prosilverData[j] === 'components' || prosilverData[j] === 'objects') {
 			newData.push(prosilverData[j]);
 		}
 	}

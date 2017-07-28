42556035c7cf99b2dd966a6a48d19aac90703f93
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-07-28 14:11:21 +0530

    corrected variables and model/controller

--- app/controller/sidebar.js
+++ app/controller/sidebar.js
@@ -1,29 +1,32 @@
 'use strict';
 
+var fs = require('fs');
 var model = require('../models/sidebar.js');
 
 var sidebar = module.exports = {};
 
-sidebar.newData = [];
+var dataAll = fs.readdirSync('tests/all/scss');
+var dataProsilver = fs.readdirSync('tests/prosilver/scss');
 
 // Logic to read an array files inside a directory
 sidebar.get = function () {
-	var allData = model.data1;
-	var prosilverData = model.data2;
+	var allData = dataAll;
+	var prosilverData = dataProsilver;
+	var localData = model.newData;
 	for (var i = 0; i < allData.length; i++) {
 		if (allData[i] === 'base' || allData[i] === 'settings' || allData[i] === 'objects' || allData[i] === 'components') {
-			sidebar.newData.push(allData[i]);
+			localData.push(allData[i]);
 		}
 	}
-	sidebar.newData.push('--------------');
+	localData.push('--------------');
 	for (var j = 0; j < prosilverData.length; j++) {
 		if (prosilverData[j] === 'theme' || prosilverData[j] === 'components' || prosilverData[j] === 'objects') {
-			sidebar.newData.push(prosilverData[j]);
+			localData.push(prosilverData[j]);
 		}
 	}
-	var swap = sidebar.newData[1];
-	sidebar.newData[1] = sidebar.newData[3];
-	sidebar.newData[3] = swap;
+	var swap = localData[1];
+	localData[1] = localData[3];
+	localData[3] = swap;
 
-	return sidebar.newData;
+	return localData;
 };
--- app/models/sidebar.js
+++ app/models/sidebar.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var fs = require('fs');
-
 var model = module.exports = {};
 
-model.data1 = fs.readdirSync('tests/all/scss');
-model.data2 = fs.readdirSync('tests/prosilver/scss');
+model.newData = [];

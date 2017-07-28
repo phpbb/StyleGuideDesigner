c0d42f20b814a6798199d19d04f1861db7c09b2d
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-07-28 23:35:27 +0530

    reversed MC

--- app/controller/sidebar.js
+++ app/controller/sidebar.js
@@ -1,32 +1,9 @@
 'use strict';
 
-var fs = require('fs');
-var model = require('../models/sidebar.js');
+var model = require('../models/sidebar');
 
 var sidebar = module.exports = {};
 
-var dataAll = fs.readdirSync('tests/all/scss');
-var dataProsilver = fs.readdirSync('tests/prosilver/scss');
-
-// Logic to read an array files inside a directory
-sidebar.get = function () {
-	var allData = dataAll;
-	var prosilverData = dataProsilver;
-	var localData = model.newData;
-	for (var i = 0; i < allData.length; i++) {
-		if (allData[i] === 'base' || allData[i] === 'settings' || allData[i] === 'objects' || allData[i] === 'components') {
-			localData.push(allData[i]);
-		}
-	}
-	localData.push('--------------');
-	for (var j = 0; j < prosilverData.length; j++) {
-		if (prosilverData[j] === 'theme' || prosilverData[j] === 'components' || prosilverData[j] === 'objects') {
-			localData.push(prosilverData[j]);
-		}
-	}
-	var swap = localData[1];
-	localData[1] = localData[3];
-	localData[3] = swap;
-
-	return localData;
+sidebar.getData = function () {
+	return model.get();
 };
--- app/models/sidebar.js
+++ app/models/sidebar.js
@@ -1,5 +1,29 @@
 'use strict';
 
 var model = module.exports = {};
+var fs = require('fs');
 
 model.newData = [];
+
+// Logic to read an array of files inside a directory
+model.get = function () {
+	var allData = fs.readdirSync('tests/all/scss');
+	var prosilverData = fs.readdirSync('tests/prosilver/scss');
+	var localData = model.newData;
+	for (var i = 0; i < allData.length; i++) {
+		if (allData[i] === 'base' || allData[i] === 'settings' || allData[i] === 'objects' || allData[i] === 'components') {
+			localData.push(allData[i]);
+		}
+	}
+	localData.push('--------------');
+	for (var j = 0; j < prosilverData.length; j++) {
+		if (prosilverData[j] === 'theme' || prosilverData[j] === 'components' || prosilverData[j] === 'objects') {
+			localData.push(prosilverData[j]);
+		}
+	}
+	var swap = localData[1];
+	localData[1] = localData[3];
+	localData[3] = swap;
+	
+	return localData;
+};
